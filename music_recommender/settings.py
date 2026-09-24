511b361dda2227209662b744d439cc548ee88bfe
Anusha <anushagarimella2003@gmail.com>
2026-09-24 20:22:30 +0000

    add sqlite3 path setting

--- music_recommender/settings.py
+++ music_recommender/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'music_recommender.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('SQLITE_DB_PATH', str(BASE_DIR / 'db.sqlite3')),
     }
 }
 
